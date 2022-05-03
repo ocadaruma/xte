86f98f1f5544784af7e5d23aa8123bce46dddcd5
Haruki Okada <ocadaruma@gmail.com>
2022-05-03 17:33:08 +0900

    scaffold

---
+++ zerotex-core/src/lib.rs
@@ -0,0 +1,52 @@
+pub mod primitive;
+
+#[derive(Debug)]
+pub struct Character {
+    value: char
+}
+
+#[derive(Debug)]
+pub enum CategoryCode {
+    Escape, // 0
+    BeginningOfGroup, // 1
+    EndOfGroup, // 2
+    MathShift, // 3
+    AlignmentTab, // 4
+    EndOfLine, // 5
+    Parameter, // 6
+    Superscript, // 7
+    Subscript, // 8
+    Ignored, // 9
+    Space, // 10
+    Letter, // 11
+    Other, // 12
+    Active, // 13
+    Comment, // 14
+    Invalid, // 15
+}
+
+#[derive(Debug)]
+pub struct Program<'a> {
+    renderer: &'a Renderer,
+    logger: &'a Logger,
+}
+
+impl Program {
+    pub fn new() -> Self {
+        unimplemented!()
+    }
+
+    pub fn advance(&mut self, c: Character) {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug)]
+pub struct Renderer {
+
+}
+
+#[derive(Debug)]
+pub struct Logger {
+
+}
---
+++ zerotex-core/src/primitive.rs
@@ -0,0 +1,49 @@
+/// \catcode
+pub struct CatCode {
+
+}
+
+/// \hsize
+pub struct HSize {
+
+}
+
+/// \vsize
+pub struct VSize {
+
+}
+
+/// \parfillskip
+pub struct ParFillSkip {
+
+}
+
+/// \font
+pub struct Font {
+
+}
+
+/// \def
+pub struct Def {
+
+}
+
+/// \kern
+pub struct Kern {
+
+}
+
+/// \lower
+pub struct Lower {
+
+}
+
+/// \hbox
+pub struct HBox {
+
+}
+
+/// \end
+pub struct End {
+
+}
